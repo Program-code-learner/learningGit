694045d99244d0059b65bc927ecbf343f9b90788
Program-code-learner <awasthigaurav173@gmail.com>
2024-10-23 18:20:37 +0530

    left-files

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,12 +1,13 @@
 // import { useAuth } from '@/context/context'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const homePage: React.FC = () => {
     // const {isAuthenticated} = useAuth();
   return (
     <div className='flex justify-center bg-slate-500 text-yellow-200  w-full h-screen'>
-    <div className=' font-bold'>homePage</div>
-    
+    <div className=' font-bold mr-5'>homePage</div>
+    <Link to={'/contact'}>contact</Link>
     </div>
   )
 }
